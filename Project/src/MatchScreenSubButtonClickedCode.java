631fbc65ed5351c1fa4344395a7f3a125d8cd9c2
Emre <emre.aslan@ug.bilkent.edu.tr>
2024-05-13 00:37:49 +0300

    SUB button activated

---
+++ Project/src/MatchScreenSubButtonClickedCode.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import javafx.fxml.FXML;
+
+public class MatchScreenSubButtonClickedCode {
+    
+}
