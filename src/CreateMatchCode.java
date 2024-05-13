70b2b1d088e4a6c4748e58a7e06f28ebde468260
Emre <emre.aslan@ug.bilkent.edu.tr>
2024-05-13 12:25:30 +0300

    FIX

--- src/CreateMatchCode.java
+++ src/CreateMatchCode.java
@@ -1,59 +1,4 @@
-<<<<<<< HEAD
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.stage.Stage;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.text.Text;
-import javafx.fxml.FXML;
 
-
-
-public class CreateMatchCode extends Application{
-
-    @FXML
-    private String nameTeamA;
-    private String nameTeamB;
-    private Team teamA;
-    private Team teamB;
-    
-
-    @FXML
-    void addPlayerForTeamA(ActionEvent event) {
-        
-    }
-
-    @FXML
-    void addPlayerForTeamB(ActionEvent event) {
-        
-    }
-
-    @FXML
-    void startMatch(ActionEvent event) {
-        System.out.println("MATCH STARTING");
-    }
-
-
-    public static void main(String[] args)  {
-        System.out.println("Hello, World!");
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-
-        Parent root = FXMLLoader.load(getClass().getResource("CreateMatch.fxml"));
-        primaryStage.setTitle("Create Match");
-        primaryStage.setScene(new Scene(root, 950, 600));
-        primaryStage.show();
-
-    
-        
-    }
-}
-=======
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.stage.Stage;
@@ -108,4 +53,4 @@ public class CreateMatchCode extends Application{
         
     }
 }
->>>>>>> f4c40ae29d90fd6b86b44ba2889febf1e1092efd
+
