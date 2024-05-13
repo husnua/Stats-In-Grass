5b29a5074c97a45c6a133bbe25de2c23d4334a79
husnua <husnuarkca00@gmail.com>
2024-05-13 16:07:02 +0300

    Team stats added

---
+++ src/TeamStats.java
@@ -0,0 +1,36 @@
+public class TeamStats {
+    private int totalGoal = 0;
+    private int totalConceded = 0;
+    private int win = 0;
+    private int loss = 0;
+    private int draw = 0;
+    private int totalMatch = 0;
+    private int yellows = 0;
+    private int reds = 0;
+    public TeamStats(){}
+    public int getTotalGoal() {
+        return totalGoal;
+    }
+    public int getTotalConceded() {
+        return totalConceded;
+    }
+    public int getWin() {
+        return win;
+    }
+    public int getLoss() {
+        return loss;
+    }
+    public int getDraw() {
+        return draw;
+    }
+    public int getTotalMatch() {
+        return totalMatch;
+    }
+    public int getYellows() {
+        return yellows;
+    }
+    public int getReds() {
+        return reds;
+    }
+
+}
